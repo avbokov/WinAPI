864ed2e5ace99ea7eea066b4d0cafc13a7f853e0
avbokov <bokov.itstep@gmail.com>
2022-02-09 21:32:09 +0700

    CW

---
+++ ComboBox/main.cpp
@@ -0,0 +1,62 @@
+﻿#include<Windows.h>
+#include<stdio.h>
+#include"resource.h"
+
+CONST CHAR* words[] =
+{
+	"This", "is", "my", "first", "Combo", "Box"
+};
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, DlgProc, 0);
+	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
+{
+	switch (uMsg)
+	{
+		case WM_INITDIALOG:
+		{
+			HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+			SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+
+			HWND hCombo = GetDlgItem(hwnd, IDC_COMBO1);
+			for (int i = 0; i < sizeof(words) / sizeof(CHAR*); i++)
+				SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)words[i]);
+		}
+			break;
+		case WM_COMMAND:
+		{
+			switch (LOWORD(wParam))
+			{
+			case IDOK:
+			{
+				CONST INT SIZE = 256;
+				CHAR buffer[SIZE]{};
+				INT cursel = SendMessage(GetDlgItem(hwnd, IDC_COMBO1), CB_GETCURSEL, 0, 0);
+				if (cursel !=CB_ERR)
+				{
+					CHAR text[SIZE]{};
+					SendMessage(GetDlgItem(hwnd, IDC_COMBO1), CB_GETLBTEXT, cursel, (LPARAM)text);
+					sprintf(buffer, "Вы выбрали элемент %d - %s", cursel, text);
+				}
+				else
+				{
+					sprintf(buffer, "Вы ничего не выбрали");
+				}
+				MessageBox(hwnd, buffer, "Info", MB_OK | MB_ICONINFORMATION);
+			}
+			case IDCANCEL: EndDialog(hwnd, 0); break;
+			}
+		
+		}
+		break;
+		case WM_CLOSE:
+			EndDialog(hwnd, 0);
+	}
+	return FALSE;
+}
--- WinAPI/main.cpp
+++ WinAPI/main.cpp
@@ -1,6 +1,9 @@
 ﻿#include<Windows.h>
 #include"resource.h"
 
+CONST CHAR INVITATION_LOGIN[] = "Введите логин";
+CONST CHAR INVITATION_PASSWORD[] = "Введите пароль";
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT cCmdShow)
@@ -19,7 +22,15 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		case WM_INITDIALOG:
 			// Инициализация окна (создание значков и т.д.)
-			break;
+		{
+			HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+			SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+			// Установка фокуса:
+			//SendMessage(GetDlgItem(hwnd, IDC_EDIT_LOGIN), WM_SETFOCUS, 0, 0);
+			SendMessage(GetDlgItem(hwnd, IDC_EDIT_LOGIN), WM_SETTEXT, 0, (LPARAM)INVITATION_LOGIN);
+			SendMessage(GetDlgItem(hwnd, IDC_EDIT_PASSWORD), WM_SETTEXT, 0, (LPARAM)INVITATION_PASSWORD);
+		}
+		break;
 		case WM_COMMAND:
 			// Обработка команд нажатия кнопок и т.д.
 		{
@@ -29,8 +40,35 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			// HIWORD - два старших Байта в двойном слове
 			switch (LOWORD(wParam))
 			{
+			case IDC_BTN_COPY:
+			{
+				CONST INT SIZE = 256;
+				CHAR buffer[SIZE]{};
+				HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_LOGIN);	// Получаем HWND элемента окна по его ResourceID
+				SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)buffer);
+				SendMessage(GetDlgItem(hwnd, IDC_EDIT_PASSWORD), WM_SETTEXT, 0, (LPARAM)buffer);
+			}
+			break;
 			case IDOK:		MessageBox(hwnd, "Была нажата кнопка ОК", "Info", MB_ICONINFORMATION | MB_OK); break;
 			case IDCANCEL:	EndDialog(hwnd, 0);	break;
+			case IDC_EDIT_LOGIN:
+			{
+				CONST INT SIZE = 256;
+				CHAR buffer[SIZE]{};
+				SendMessage(GetDlgItem(hwnd, IDC_EDIT_LOGIN), WM_GETTEXT, SIZE, (LPARAM)buffer);
+				switch (HIWORD(wParam))
+				{
+				case EN_SETFOCUS:
+					if (strcmp(buffer, INVITATION_LOGIN)==0)
+						SendMessage(GetDlgItem(hwnd, IDC_EDIT_LOGIN), WM_SETTEXT, 0, (LPARAM)"");
+					break;
+				case EN_KILLFOCUS:
+					if (strcmp(buffer, "") == 0)
+						SendMessage(GetDlgItem(hwnd, IDC_EDIT_LOGIN), WM_SETTEXT, 0, (LPARAM)INVITATION_LOGIN);
+					break;
+				}
+			}
+			break;
 			}
 		}
 			break;
